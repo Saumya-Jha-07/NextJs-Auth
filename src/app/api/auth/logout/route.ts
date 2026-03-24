83eee18dfe613704eafe349e5f5b680e7b278ef8
Saumya Jha <jhasaumya554@gmail.com>
2026-03-25 04:15:52 +0530

    feat : logout

---
+++ src/app/api/auth/logout/route.ts
@@ -0,0 +1,39 @@
+import { User } from "@/models/user.model";
+import connectDB from "@/utils/db";
+import { NextRequest, NextResponse } from "next/server";
+import jwt from "jsonwebtoken";
+
+connectDB();
+
+// export const runtime = "nodejs";
+
+export async function POST(request: NextRequest) {
+  const accessToken = request.cookies.get("accessToken")?.value;
+
+  if (!accessToken) {
+    return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+  }
+
+  const decodedToken: any = jwt.verify(
+    accessToken,
+    process.env.ACCESS_TOKEN_SECRET!,
+  );
+
+  const user = await User.findById(decodedToken._id);
+
+  await User.findByIdAndUpdate(user._id, {
+    $unset: {
+      refreshToken: 1,
+    },
+  });
+
+  const res = NextResponse.json(
+    { message: "User logout successfully!" },
+    { status: 200 },
+  );
+
+  res.cookies.delete("accessToken");
+  res.cookies.delete("refreshToken");
+
+  return res;
+}
