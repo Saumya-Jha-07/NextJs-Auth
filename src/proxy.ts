404db8ed3d9c298ab14b05589c1ef6024f2845dc
Saumya Jha <jhasaumya554@gmail.com>
2026-03-26 11:52:36 +0530

    test : proxy

---
+++ src/proxy.ts
@@ -0,0 +1,11 @@
+// import { NextRequest, NextResponse } from "next/server";
+
+// export function proxy(request: NextRequest) {
+//   console.log("proxy pe req aayi , redirecting to /login");
+
+//   return NextResponse.redirect(new URL("/login", request.url));
+// }
+
+// export const config = {
+//   matcher: ["/signup "],
+// };
